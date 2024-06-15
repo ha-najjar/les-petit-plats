113c722f1d01bc702471a06a761045819d2698db
najar <hajer.najar.ing@gmail.com>
2024-06-15 03:05:19 +0200

    quelques corrections sur la recherche avancée en utilisant les boucle native

--- script/pages/index.js
+++ script/pages/index.js
@@ -1,82 +1,3 @@
-// Fonction pour filtrer les recettes basées sur la recherche de l'utilisateur en utilisant des boucles native
-function filterRecipes(recipes, selectedItemsValues) {
-  const filteredRecipes = [];
-
-  for (let i = 0; i < recipes.length; i++) {
-    const recipe = recipes[i];
-    const recipeName = recipe.name.toLowerCase();
-    const recipeDescription = recipe.description.toLowerCase();
-
-    // Convertir les ingrédients en minuscules
-    const recipeIngredients = [];
-    for (let j = 0; j < recipe.ingredients.length; j++) {
-      recipeIngredients.push(recipe.ingredients[j].ingredient.toLowerCase());
-    }
-
-    // Convertir l'appareil en minuscules
-    const recipeAppliance = recipe.appliance.toLowerCase();
-
-    // Convertir les ustensiles en minuscules
-    const recipeUstensils = [];
-    for (let j = 0; j < recipe.ustensils.length; j++) {
-      recipeUstensils.push(recipe.ustensils[j].toLowerCase());
-    }
-
-    let allItemsMatch = true;
-
-    for (let k = 0; k < selectedItemsValues.length; k++) {
-      const selectedItem = selectedItemsValues[k].toLowerCase();
-
-      // Ignorer les éléments vides
-      if (selectedItem.trim() === "") {
-        continue;
-      }
-
-      let ingredientMatch = false;
-      let applianceMatch = false;
-      let ustensilMatch = false;
-
-      // Vérifier les ingrédients
-      for (let j = 0; j < recipeIngredients.length; j++) {
-        if (recipeIngredients[j].includes(selectedItem)) {
-          ingredientMatch = true;
-          break;
-        }
-      }
-
-      // Vérifier l'appareil
-      if (recipeAppliance.includes(selectedItem)) {
-        applianceMatch = true;
-      }
-
-      // Vérifier les ustensiles
-      for (let j = 0; j < recipeUstensils.length; j++) {
-        if (recipeUstensils[j].includes(selectedItem)) {
-          ustensilMatch = true;
-          break;
-        }
-      }
-
-      if (
-        !recipeName.includes(selectedItem) &&
-        !recipeDescription.includes(selectedItem) &&
-        !ingredientMatch &&
-        !applianceMatch &&
-        !ustensilMatch
-      ) {
-        allItemsMatch = false;
-        break;
-      }
-    }
-
-    if (allItemsMatch) {
-      filteredRecipes.push(recipe);
-    }
-  }
-
-  return filteredRecipes;
-}
-
 // Fonction pour mettre à jour l'affichage des recettes et le nombre de recettes
 function updateDisplay(filteredRecipes) {
   // Vider la section des recettes
@@ -145,20 +66,20 @@ function updateSearchResults() {
 }
 
 async function init() {
-  document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("DOMContentLoaded", async () => {
     // Appeler la fonction pour afficher les données des recettes
     displayData(recipes);
     // Appeler la fonction pour créer et remplir les dropdowns
-    createDropdowns(recipes);
+    await createDropdowns(recipes);
     recipesNumber(recipes);
 
     // Ajouter un écouteur d'événement pour le bouton de recherche
-    const searchButton = document.querySelector("#search-button");
+    //const searchButton = document.querySelector("#search-button");
     const searchInput = document.querySelector("#search-input");
 
     // Ajouter un écouteur d'événement pour le bouton de recherche
-    searchButton.addEventListener("click", (event) => {
-      event.preventDefault();
+    searchInput.addEventListener("input", () => {
+      //event.preventDefault();
       updateSearchResults();
     });
   });
